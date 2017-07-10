c45fb1de98dcbe209e1a32a3da16ef6319a005d2
Mari0 <mario.rosenstengel@rwth-aachen.de>
2017-07-10 12:50:04 +0200

    guidance modeling works again

--- src/js/Guidancemodel.js
+++ src/js/Guidancemodel.js
@@ -7,122 +7,112 @@ define([
      * Guidancemodel
      * @name Guidancemodel
      */
-    function Guidancemodel(){
+    function Guidancemodel() {
         var guidancemodeling = {};
 
         var deferred = $.Deferred();
-        //Check whether this is the guidance modeling editor based on the activity name
-        //var act = openapp.param.get("http://purl.org/role/terms/activity");
-        //openapp.resource.get(act, function(resource){
-            var resource = {};//delete this line and uncomment the first to line above to enable guidance modeling, requires also openapp
-            var activityName;
-            console.info('Guidance promise by ' + frameElement.name, resource);
-            try {
-                if (resource.data)
-                    activityName = resource.data[resource.uri]["http://purl.org/dc/terms/title"][0].value;
-                else
-                    activityName = "";
-            }catch(e){
-                console.info('Guidancemodel promise failed!');
-            }
-            guidancemodeling.INITIAL_NODE_LABEL = "Initial node";
-            guidancemodeling.MERGE_NODE_LABEL = "Decision node";
-            guidancemodeling.CALL_ACTIVITY_NODE_LABEL = "Call activity node";
-            guidancemodeling.ACTIVITY_FINAL_NODE_LABEL = "Activity final node";
-            guidancemodeling.CONCURRENCY_NODE_LABEL = "Fork node";
 
-            guidancemodeling.isGuidanceEditor = function(){
-                return activityName == "Guidance modeling";
-            };
+        var activityName = $('.activityEntry.activitySel', parent.document).find('.activity-title').text();
 
-            guidancemodeling.getCreateObjectNodeLabelForType = function(type){
-                return "Create " + type + " object";
-            };
+        console.info('Guidance promise by ' + frameElement.name + ' Activity:' + activityName);
+        guidancemodeling.INITIAL_NODE_LABEL = "Initial node";
+        guidancemodeling.MERGE_NODE_LABEL = "Decision node";
+        guidancemodeling.CALL_ACTIVITY_NODE_LABEL = "Call activity node";
+        guidancemodeling.ACTIVITY_FINAL_NODE_LABEL = "Activity final node";
+        guidancemodeling.CONCURRENCY_NODE_LABEL = "Fork node";
 
-            guidancemodeling.isCreateObjectNodeLabel = function(label){
-                var match = /Create (.*?) object/.exec(label);
-                if(match)
-                    return match[1];
-                else
-                    return "";
-            };
+        guidancemodeling.isGuidanceEditor = function () {
+            return activityName == "Guidance modeling";
+        };
 
-            guidancemodeling.getCreateRelationshipNodeLabelForType = function(type){
-                return "Create " + type + " relationship";
-            };
+        guidancemodeling.getCreateObjectNodeLabelForType = function (type) {
+            return "Create " + type + " object";
+        };
 
-            guidancemodeling.isCreateRelationshipNodeLabel = function(label){
-                var match = /Create (.*?) relationship/.exec(label);
-                if(match)
-                    return match[1];
-                else
-                    return "";
-            };
+        guidancemodeling.isCreateObjectNodeLabel = function (label) {
+            var match = /Create (.*?) object/.exec(label);
+            if (match)
+                return match[1];
+            else
+                return "";
+        };
 
-            guidancemodeling.getSetPropertyNodeLabelForType = function(type){
-                return "Set property for " + type;
-            };
+        guidancemodeling.getCreateRelationshipNodeLabelForType = function (type) {
+            return "Create " + type + " relationship";
+        };
 
-            guidancemodeling.isSetPropertyNodeLabel = function(label){
-                var match = /Set property for (.*)/.exec(label);
-                if(match)
-                    return match[1];
-                else
-                    return "";
-            };
+        guidancemodeling.isCreateRelationshipNodeLabel = function (label) {
+            var match = /Create (.*?) relationship/.exec(label);
+            if (match)
+                return match[1];
+            else
+                return "";
+        };
 
-            guidancemodeling.getEntityNodeLabelForType = function(type){
-                return type + " entity";
-            };
+        guidancemodeling.getSetPropertyNodeLabelForType = function (type) {
+            return "Set property for " + type;
+        };
 
-            guidancemodeling.isEntityNodeLabel = function(label){
-                var match = /(.*?) entity/.exec(label);
-                if(match)
-                    return match[1];
-                else
-                    return "";
-            };
+        guidancemodeling.isSetPropertyNodeLabel = function (label) {
+            var match = /Set property for (.*)/.exec(label);
+            if (match)
+                return match[1];
+            else
+                return "";
+        };
 
-            guidancemodeling.getObjectContextLabelForType = function(type){
-                return type + " Object Context";
-            };
+        guidancemodeling.getEntityNodeLabelForType = function (type) {
+            return type + " entity";
+        };
 
-            guidancemodeling.getObjectTypeForObjectContextType = function(type){
-                var i = type.lastIndexOf(" Object Context");
-                return type.substring(0, i);
-            };
+        guidancemodeling.isEntityNodeLabel = function (label) {
+            var match = /(.*?) entity/.exec(label);
+            if (match)
+                return match[1];
+            else
+                return "";
+        };
 
-            guidancemodeling.isObjectContextType = function(type){
-                return type.indexOf(" Object Context", type.length - " Object Context".length) !== -1;
-            };
+        guidancemodeling.getObjectContextLabelForType = function (type) {
+            return type + " Object Context";
+        };
 
-            guidancemodeling.getRelationshipContextLabelForType = function(type){
-                return type + " Relationship Context";
-            };
+        guidancemodeling.getObjectTypeForObjectContextType = function (type) {
+            var i = type.lastIndexOf(" Object Context");
+            return type.substring(0, i);
+        };
 
-            guidancemodeling.getRelationshipTypeForRelationshipContextType = function(type){
-                var i = type.lastIndexOf(" Relationship Context");
-                return type.substring(0, i);
-            };
+        guidancemodeling.isObjectContextType = function (type) {
+            return type.indexOf(" Object Context", type.length - " Object Context".length) !== -1;
+        };
 
-            guidancemodeling.isRelationshipContextType = function(type){
-                return type.indexOf(" Relationship Context", type.length - " Relationship Context".length) !== -1;
-            };
+        guidancemodeling.getRelationshipContextLabelForType = function (type) {
+            return type + " Relationship Context";
+        };
 
-            guidancemodeling.getObjectToolLabelForType = function(type){
-                return type + " Tool";
-            };
+        guidancemodeling.getRelationshipTypeForRelationshipContextType = function (type) {
+            var i = type.lastIndexOf(" Relationship Context");
+            return type.substring(0, i);
+        };
 
-            guidancemodeling.getObjectTypeForObjectToolType = function(type){
-                var i = type.lastIndexOf(" Tool");
-                return type.substring(0, i);
-            };
+        guidancemodeling.isRelationshipContextType = function (type) {
+            return type.indexOf(" Relationship Context", type.length - " Relationship Context".length) !== -1;
+        };
 
-            guidancemodeling.isObjectToolType = function(type){
-                return type.indexOf(" Tool", type.length - " Tool".length) !== -1;
-            };
+        guidancemodeling.getObjectToolLabelForType = function (type) {
+            return type + " Tool";
+        };
 
-            deferred.resolve(guidancemodeling);
+        guidancemodeling.getObjectTypeForObjectToolType = function (type) {
+            var i = type.lastIndexOf(" Tool");
+            return type.substring(0, i);
+        };
+
+        guidancemodeling.isObjectToolType = function (type) {
+            return type.indexOf(" Tool", type.length - " Tool".length) !== -1;
+        };
+
+        deferred.resolve(guidancemodeling);
 
         //});
         return deferred.promise();
