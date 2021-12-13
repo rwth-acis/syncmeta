b095bea376b2b3e95ceae98c972c25014afa4f71
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-12-13 12:39:08 +0100

    create slim debug widget for sbf

--- widgets/Gruntfile.js
+++ widgets/Gruntfile.js
@@ -266,6 +266,17 @@ module.exports = function(grunt) {
                 }
             },
 
+            debug_widget_slim_html: {
+                options: {
+                    data: {
+                        bodyPartial: '_debug_widget_slim.tpl' 
+                    }
+                },
+                files: {
+                    'html/debug_slim.html': ['<%= srcdir %>/widgets/widget.html.tpl']
+                }
+            },
+
             imsld_export_widget: {
                 options: {
                     data: {
