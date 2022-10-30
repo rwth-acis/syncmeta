857f35df7bfc852eaf6092b6a2fd0482f97a318f
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-10-30 16:48:59 +0100

    wip

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -1,9 +1,6 @@
 // Import rollup plugins
 import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
-import { copy } from "@web/rollup-plugin-copy";
 import resolve from "@rollup/plugin-node-resolve";
-// import { terser } from "rollup-plugin-terser";
-// import minifyHTML from "rollup-plugin-minify-html-literals";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import css from "rollup-plugin-import-css";
@@ -16,37 +13,24 @@ const pkg = require("./package.json");
 export default {
   plugins: [
     // Resolve bare module specifiers to relative paths
-    resolve({ browser: true }),
+    resolve({ browser: true, mainFields: ["module", "browser"] }),
     commonjs({
       include: ["node_modules/**"],
       transformMixedEsModules: true,
       extensions: [".js", ".ts"],
     }), // makes sure that any commonjs modules are transformed to es6 to be bundled the ".ts" extension is required
-    typescript({ compilerOptions: { module: "CommonJS" } }),
+    typescript(),
     css(),
     // Entry point for application build; can specify a glob to build multiple
     // HTML files for non-SPA app
     html({
       input: "index.html",
     }),
-
-    // Minify HTML template literals
-    // minifyHTML(),
-    // Minify JS
-    // terser({
-    //   ecma: 2020,
-    //   module: true,
-    //   warnings: true,
-    // }),
-    // // Optional: copy any static assets to build directory
-    // copy({
-    //   patterns: ["images/**/*"],
-    // }),
   ],
   output: {
     dir: "build",
     sourcemap: true,
-    format: "esm",
+    format: "iife",
     globals: {
       jquery: "$",
       lit: "lit",
