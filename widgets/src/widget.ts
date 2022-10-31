38e2ce146505082b874b4d1ced8ab038d65b7428
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-10-31 22:10:02 +0100

    add styles directly in component

--- widgets/src/widget.ts
+++ widgets/src/widget.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, CSSResultGroup, css } from "lit";
 import { classMap } from "lit/directives/class-map.js";
 import { customElement, property } from "lit/decorators.js";
 import * as Y from "yjs";
@@ -14,6 +14,35 @@ export const SyncMetaWidget = <T extends Constructor<LitElement>>(
   // cannot use arrow function here, see https://lit.dev/docs/composition/mixins/#applying-decorators-in-mixins
   class SyncMetaWidgetElement extends superClass {
     @property({ type: String }) widgetName = "SyncMetaWidget";
+    static styles?: CSSResultGroup = css`
+      .loading {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        /*noinspection CssUnknownTarget*/
+        background: white url("/img/loading.gif") no-repeat center center;
+        z-index: 32032;
+        opacity: 0.75;
+      }
+
+      #oauthPersonalize,
+      #oauthPersonalizeDone,
+      #oauthPersonalizeComplete {
+        position: relative;
+        z-index: 32033;
+      }
+
+      #q {
+        position: absolute;
+        width: 100%;
+        height: 3px;
+        bottom: 0;
+        left: 0;
+        cursor: s-resize;
+      }
+    `;
 
     connectedCallback() {
       super.connectedCallback();
