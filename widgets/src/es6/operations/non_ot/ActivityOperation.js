8d7c2aa812d0a2ddbd9d1f85966040b7b95362b2
lakhoune <a.b.lakhoune@gmail.com>
2022-12-12 10:43:28 +0100

    transform to class

--- widgets/src/es6/operations/non_ot/ActivityOperation.js
+++ widgets/src/es6/operations/non_ot/ActivityOperation.js
@@ -1,129 +1,129 @@
-import NonOTOperation from './NonOTOperation';
+import NonOTOperation from "./NonOTOperation";
 
-    ActivityOperation.TYPE = "ActivityOperation";
+/**
+ * ActivityOperation
+ * @class operations.non_ot.ActivityOperation
+ * @memberof operations.non_ot
+ * @constructor
+ * @param {string} type Type of activity
+ * @param {string} entityId Entity id of the entity this activity works on
+ * @param {string} sender JabberId of the user who issued this activity
+ * @param {string} text Text of this activity which is displayed in the activity widget
+ * @param {object} data Additional data for the activity
+ */
+class ActivityOperation {
+  static TYPE = "ActivityOperation";
+  constructor(type, entityId, sender, text, data) {
+    /**
+     * Type of activity
+     * @type {string}
+     * @private
+     */
+    var _type = type;
 
     /**
-     * ActivityOperation
-     * @class operations.non_ot.ActivityOperation
-     * @memberof operations.non_ot
-     * @constructor
-     * @param {string} type Type of activity
-     * @param {string} entityId Entity id of the entity this activity works on
-     * @param {string} sender JabberId of the user who issued this activity
-     * @param {string} text Text of this activity which is displayed in the activity widget
-     * @param {object} data Additional data for the activity
+     * Entity id of the entity this activity works on
+     * @type {string}
+     * @private
      */
-    function ActivityOperation(type,entityId,sender,text,data){
-        /**
-         * Type of activity
-         * @type {string}
-         * @private
-         */
-        var _type = type;
+    var _entityId = entityId;
 
-        /**
-         * Entity id of the entity this activity works on
-         * @type {string}
-         * @private
-         */
-        var _entityId = entityId;
+    /**
+     * JabberId of the user who issued this activity
+     * @type {string}
+     * @private
+     */
+    var _sender = sender;
 
-        /**
-         * JabberId of the user who issued this activity
-         * @type {string}
-         * @private
-         */
-        var _sender = sender;
+    /**
+     * Text of this activity which is displayed in the activity widget
+     * @type {string}
+     * @private
+     */
+    var _text = text;
 
-        /**
-         * Text of this activity which is displayed in the activity widget
-         * @type {string}
-         * @private
-         */
-        var _text = text;
+    /**
+     * Additional data for the activity
+     * @type {Object}
+     * @private
+     */
+    var _data = data;
 
-        /**
-         * Additional data for the activity
-         * @type {Object}
-         * @private
-         */
-        var _data = data;
+    /**
+     * Corresponding NonOtOperation
+     * @type {operations.non_ot.NonOTOperation}
+     * @private
+     */
+    var _nonOTOperation = null;
 
-        /**
-         * Corresponding NonOtOperation
-         * @type {operations.non_ot.NonOTOperation}
-         * @private
-         */
-        var _nonOTOperation = null;
+    /**
+     * Get type of activity
+     * @returns {string}
+     */
+    this.getType = function () {
+      return _type;
+    };
 
-        /**
-         * Get type of activity
-         * @returns {string}
-         */
-        this.getType = function(){
-            return _type;
-        };
+    /**
+     * Get entity id of the entity this activity works on
+     * @returns {string}
+     */
+    this.getEntityId = function () {
+      return _entityId;
+    };
 
-        /**
-         * Get entity id of the entity this activity works on
-         * @returns {string}
-         */
-        this.getEntityId = function(){
-            return _entityId;
-        };
+    /**
+     * Get JabberId of the user who issued this activity
+     * @returns {string}
+     */
+    this.getSender = function () {
+      return _sender;
+    };
 
-        /**
-         * Get JabberId of the user who issued this activity
-         * @returns {string}
-         */
-        this.getSender = function(){
-            return _sender;
-        };
+    /**
+     * Get text of this activity which is displayed in the activity widget
+     * @returns {string}
+     */
+    this.getText = function () {
+      return _text;
+    };
 
-        /**
-         * Get text of this activity which is displayed in the activity widget
-         * @returns {string}
-         */
-        this.getText = function(){
-            return _text;
-        };
+    /**
+     * Get additional data for the activity
+     * @returns {Object}
+     */
+    this.getData = function () {
+      return _data;
+    };
 
-        /**
-         * Get additional data for the activity
-         * @returns {Object}
-         */
-        this.getData = function(){
-            return _data;
-        };
-
-        /**
-         * Convert operation to NonOTOperation
-         * @returns {operations.non_ot.NonOTOperation}
-         */
-        this.toNonOTOperation = function(){
-            if(_nonOTOperation === null){
-                _nonOTOperation = new NonOTOperation(
-                    ActivityOperation.TYPE,
-                    JSON.stringify({
-                        type: _type,
-                        entityId : _entityId,
-                        sender: _sender,
-                        text: _text,
-                        data: _data
-                    })
-                );
-            }
-            return _nonOTOperation;
-        };
-    }
-    ActivityOperation.prototype.toJSON = function(){
-        return {
-            type: this.getType(),
-            entityId: this.getEntityId(),
-            sender: this.getSender(),
-            text: this.getText(),
-            data: this.getData()
-        }
-    }
-    export default ActivityOperation;
-
+    /**
+     * Convert operation to NonOTOperation
+     * @returns {operations.non_ot.NonOTOperation}
+     */
+    this.toNonOTOperation = function () {
+      if (_nonOTOperation === null) {
+        _nonOTOperation = new NonOTOperation(
+          ActivityOperation.TYPE,
+          JSON.stringify({
+            type: _type,
+            entityId: _entityId,
+            sender: _sender,
+            text: _text,
+            data: _data,
+          })
+        );
+      }
+      return _nonOTOperation;
+    };
+  }
+  toJSON() {
+    return {
+      type: this.getType(),
+      entityId: this.getEntityId(),
+      sender: this.getSender(),
+      text: this.getText(),
+      data: this.getData(),
+    };
+  }
+}
+export default ActivityOperation;
