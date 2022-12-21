46cbc6604ed2b7118e0279e388bf5daa32049b51
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-12-21 10:32:54 +0100

    add comment to explain how html loader works

--- widgets/src/es6/html.template.loader.js
+++ widgets/src/es6/html.template.loader.js
@@ -1,4 +1,5 @@
 // from https://marian-caikovski.medium.com/how-to-import-html-template-file-into-javascript-module-265746167974
+// loadHTML function is replaced by the corresponding html string using the rollup plugin widgets\importmap.plugin.js
 export default function (htmlRelativeUrl, baseUrl) {
   const htmlUrl = new URL(htmlRelativeUrl, baseUrl).href;
   return fetch(htmlUrl).then((response) => response.text());
