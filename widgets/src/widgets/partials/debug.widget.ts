1eb487a52af8210839fa68d47cd8d20f30ae3859
lakhoune <a.b.lakhoune@gmail.com>
2022-12-27 15:24:06 +0100

    concise headers, blue import buttons

--- widgets/src/widgets/partials/debug.widget.ts
+++ widgets/src/widgets/partials/debug.widget.ts
@@ -38,85 +38,81 @@ export class DebugWidget extends SyncMetaWidget(LitElement) {
         </div>
 
         <hr />
-        <div id="modelDiv" class="seperating_box">
-          <h6>
-            Import/Export/Delete a <strong>(Meta- or Guidance-)Model</strong>
-          </h6>
-          <button
-            id="import-model"
-            class="btn btn-secondary"
-            title="Import a model to the canvas"
-          >
-            Import
-          </button>
-          <button
-            id="export-model"
-            class="btn btn-secondary"
-            title="export the model as JSON"
-          >
-            Export
-          </button>
-          <button
-            id="delete-model"
-            title="delete the model"
-            class="btn btn-danger"
-          >
-            Delete
-          </button>
-        </div>
-        <hr />
-        <div id="vlsDiv" class="seperating_box">
-          <h6>
-            Import/Export/Delete a <strong>Metamodel</strong> (Model Editor
-            only)
-          </h6>
-          <button
-            class="btn btn-secondary"
-            id="import-meta-model"
-            title="Refresh the role space to apply the new VLS."
-          >
-            Import
-          </button>
-          <button
-            id="export-meta-model"
-            title="Download the VLS as JSON"
-            class="btn btn-secondary"
-          >
-            Export
-          </button>
-          <button
-            id="delete-meta-model"
-            title="Refresh the role space and delete the current modeling language"
-            class="btn btn-danger"
-          >
-            Delete
-          </button>
-        </div>
-        <hr />
-        <div id="guidanceDiv" class="seperating_box">
-          <h6>
-            Import/Export/Delete a <strong>Logical Guidancemodel</strong> (Model
-            Editor only)
-          </h6>
-          <button id="import-guidance-model" class="btn btn-secondary">
-            Import
-          </button>
-          <button id="export-guidance-model" class="btn btn-secondary">
-            Export
-          </button>
-          <button id="delete-guidance-model" class="btn btn-danger">
-            Delete
-          </button>
-        </div>
-        <hr />
-        <div id="activityDiv" class="seperating_box">
-          <h6>Export/Delete a <strong>Activity list</strong></h6>
-          <button id="export-activity-list" class="btn btn-secondary">
-            Export
-          </button>
-          <button id="delete-activity-list" class="btn btn-danger">
-            Delete
-          </button>
+        <div id="import-export-container">
+          <div id="modelDiv" class="seperating_box">
+            <h6>
+              <strong>(Meta- or Guidance-)Model</strong>
+            </h6>
+            <button
+              id="import-model"
+              class="btn btn-primary"
+              title="Import a model to the canvas"
+            >
+              Import
+            </button>
+            <button
+              id="export-model"
+              class="btn btn-secondary"
+              title="export the model as JSON"
+            >
+              Export
+            </button>
+            <button
+              id="delete-model"
+              title="delete the model"
+              class="btn btn-danger"
+            >
+              Delete
+            </button>
+          </div>
+          <hr />
+          <div id="vlsDiv" class="seperating_box">
+            <h6><strong>Metamodel</strong> (Model Editor only)</h6>
+            <button
+              class="btn btn-primary"
+              id="import-meta-model"
+              title="Refresh the role space to apply the new VLS."
+            >
+              Import
+            </button>
+            <button
+              id="export-meta-model"
+              title="Download the VLS as JSON"
+              class="btn btn-secondary"
+            >
+              Export
+            </button>
+            <button
+              id="delete-meta-model"
+              title="Refresh the role space and delete the current modeling language"
+              class="btn btn-danger"
+            >
+              Delete
+            </button>
+          </div>
+          <hr />
+          <div id="guidanceDiv" class="seperating_box">
+            <h6><strong>Logical Guidancemodel</strong> (Model Editor only)</h6>
+            <button id="import-guidance-model" class="btn btn-primary">
+              Import
+            </button>
+            <button id="export-guidance-model" class="btn btn-secondary">
+              Export
+            </button>
+            <button id="delete-guidance-model" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+          <hr />
+          <div id="activityDiv" class="seperating_box">
+            <h6><strong>Activity list</strong></h6>
+            <button id="export-activity-list" class="btn btn-secondary">
+              Export
+            </button>
+            <button id="delete-activity-list" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
         </div>
         <loading-spinner></loading-spinner>
       </div>
