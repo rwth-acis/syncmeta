288685fbb24cc7df6a9d1f68d9e00305d88b2f7f
lakhoune <ben.lakhoune@rwth-aachen.de>
2023-01-18 16:34:35 +0100

    Revert "fix node not selected when clicked"
    This reverts commit de8f5b59710b390011d7a29e4d333ebee03ff9f0.

--- widgets/src/es6/canvas_widget/Manager.js
+++ widgets/src/es6/canvas_widget/Manager.js
@@ -1566,7 +1566,7 @@ export class AbstractNode extends AbstractEntity {
      */
     var _$node = $(_.template(abstractNodeHtml)({ id: id }));
 
-    var nodeSelector = getQuerySelectorFromNode(_$node[0]);
+    this.nodeSelector = getQuerySelectorFromNode(_$node[0]);
 
     var _$awarenessTrace = $(
       _.template(awarenessTraceHtml)({ id: id + "_awareness" })
@@ -2668,10 +2668,6 @@ export class AbstractNode extends AbstractEntity {
 
       jsPlumbInstance.manage(_$node.get(0));
 
-      _$node.on("mousedown", function (e) {
-        _canvas.select(that);
-      });
-
       jsPlumbInstance.bind(EVENT_DRAG_START, function (params) {
         _canvas.hideGuidanceBox();
         _$node.css({ opacity: 0.5 });
@@ -2758,7 +2754,7 @@ export class AbstractNode extends AbstractEntity {
      */
     this.makeSource = function () {
       _$node.addClass("source");
-      window.jsPlumbInstance.addSourceSelector(nodeSelector, {
+      window.jsPlumbInstance.addSourceSelector(this.nodeSelector, {
         connectorPaintStyle: { fill: "black", strokeWidth: 4 },
         endpoint: "Dot",
         anchor: _anchorOptions,
@@ -2781,7 +2777,7 @@ export class AbstractNode extends AbstractEntity {
      */
     this.makeTarget = function () {
       _$node.addClass("target");
-      window.jsPlumbInstance.addTargetSelector(nodeSelector, {
+      window.jsPlumbInstance.addTargetSelector(this.nodeSelector, {
         isTarget: false,
         endpoint: "Dot",
         anchor: _anchorOptions,
@@ -2807,8 +2803,8 @@ export class AbstractNode extends AbstractEntity {
     this.unbindEdgeToolEvents = function () {
       try {
         _$node.removeClass("source target");
-        window.jsPlumbInstance.removeSourceSelector(nodeSelector);
-        window.jsPlumbInstance.removeTargetSelector(nodeSelector);
+        window.jsPlumbInstance.removeSourceSelector(this.nodeSelector);
+        window.jsPlumbInstance.removeTargetSelector(this.nodeSelector);
       } catch (error) {
         console.error(error);
       }
@@ -2866,6 +2862,7 @@ export class AbstractNode extends AbstractEntity {
       });
     };
   }
+  nodeSelector;
   /**
    * Apply position and dimension attributes to the node
    */
@@ -5514,7 +5511,7 @@ export function makeNode(type, $shape, anchors, attributes) {
        */
       this.makeSource = function () {
         _$node.addClass("source");
-        window.jsPlumbInstance.addSourceSelector(nodeSelector, {
+        window.jsPlumbInstance.addSourceSelector(this.nodeSelector, {
           connectorPaintStyle: { fill: "black", lineWidth: 4 },
           endpoint: "Dot",
           anchor: _anchorOptions,
@@ -5542,7 +5539,7 @@ export function makeNode(type, $shape, anchors, attributes) {
        */
       this.makeTarget = function () {
         _$node.addClass("target");
-        window.jsPlumbInstance.addTargetSelector(nodeSelector, {
+        window.jsPlumbInstance.addTargetSelector(this.nodeSelector, {
           isTarget: false,
           uniqueEndpoint: false,
           endpoint: "Dot",
@@ -5584,8 +5581,8 @@ export function makeNode(type, $shape, anchors, attributes) {
       this.unbindEdgeToolEvents = function () {
         try {
           _$node.removeClass("source target");
-          window.jsPlumbInstance.removeSourceSelector(nodeSelector);
-          window.jsPlumbInstance.removeTargetSelector(nodeSelector);
+          window.jsPlumbInstance.removeSourceSelector(this.nodeSelector);
+          window.jsPlumbInstance.removeTargetSelector(this.nodeSelector);
         } catch (error) {
           console.error(error);
         }
