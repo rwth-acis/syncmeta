8effd9a368895b2265e3a35f66f0254c2ca36d73
lakhoune <a.b.lakhoune@gmail.com>
2023-01-31 16:10:45 +0100

    add yjs sync to index

--- widgets/index.js
+++ widgets/index.js
@@ -1,6 +1,5 @@
 import "./build/widgets/widget.container";
 
 export { getWidgetTagName } from "./src/es6/config";
-
-import IWCWrapper from "./src/es6/lib/IWCWrapper";
 export { IWCWrapper };
+export { yjsSync } from "./src/es6/lib/yjs-sync";
