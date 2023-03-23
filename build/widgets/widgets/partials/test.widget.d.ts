923e67264a5c86a9cdbd4b7f11db57f67e2f5a44
lakhoune <a.b.lakhoune@gmail.com>
2023-03-23 19:45:08 +0100

    remove build

--- build/widgets/widgets/partials/test.widget.d.ts
+++
@@ -1,9 +0,0 @@
-import { LitElement } from "lit";
-declare const TestWidget_base: typeof LitElement;
-export declare class TestWidget extends TestWidget_base {
-    render(): import("lit-html").TemplateResult<1>;
-    static styles: import("lit").CSSResult;
-    connectedCallback(): void;
-    disconnectedCallback(): void;
-}
-export {};
