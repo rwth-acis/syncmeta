609c66a5607736d34fbce975ac6d7b7582815136
lakhoune <a.b.lakhoune@gmail.com>
2023-04-14 16:27:44 +0200

    implements event listener for ytype changes

--- src/es6/canvas_widget/MultiValue.js
+++ src/es6/canvas_widget/MultiValue.js
@@ -2,7 +2,7 @@ import "https://unpkg.com/jquery@3.6.0/dist/jquery.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js";
 import _ from "lodash-es";
 import AbstractValue from "./AbstractValue";
-import { Map as YMap } from "yjs";
+import { Map as YMap, YMapEvent, YTextEvent } from "yjs";
 
 /**
  * Value
@@ -111,16 +111,51 @@ export class MultiValue extends AbstractValue {
     }
 
     this._ymap.observeDeep(([event]) => {
-      console.log(event);
-      event.keysChanged.forEach((key) => {
-        const value = event.target.get(key);
+      if (event instanceof YTextEvent) {
+        // case where a value in one of the YText is updated
+        console.log(event);
+        const editorId = event.path[0];
+        const value = event.target.toString().trim();
         // update the value
-        this._value[key] = value;
-        // find the corresponding list item
-        const $li = this._$node.find(`li:contains(${key})`);
-        // update the value
-        $li.text(`${key}: ${value}`);
-      });
+        this._value[editorId] = value;
+        // update the list item
+        this._$node
+          .find(`li:contains(${editorId})`)
+          .text(`${editorId}: ${value}`);
+      } else if (event instanceof YMapEvent) {
+        // case where a value is added, deleted or updated
+        Array.from(event.changes.keys.entries()).forEach(
+          ([key, { action }]) => {
+            const value = event.target.get(key);
+            // update the value
+            this._value[key] = value;
+            switch (action) {
+              case "add":
+                // add the new list item
+                this._$node.append(
+                  $(
+                    _.template(`<li><%= key %>: <%= value %></li>`)({
+                      key,
+                      value,
+                    })
+                  )
+                );
+                break;
+              case "delete":
+                this._$node.find(`li:contains(${key})`).remove();
+                break;
+
+              case "update":
+                this._$node
+                  .find(`li:contains(${key})`)
+                  .text(`${key}: ${value}`);
+                break;
+              default:
+                break;
+            }
+          }
+        );
+      }
     });
   }
 
