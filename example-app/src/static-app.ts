cf844c2ac4da5e74afac195d2d729dd83188bb89
lakhoune <a.b.lakhoune@gmail.com>
2023-05-11 12:16:03 +0200

    example app uses unminified for better debugging

--- example-app/src/static-app.ts
+++ example-app/src/static-app.ts
@@ -11,7 +11,7 @@ import { Common } from "./common";
 import Static from "./static";
 import * as IWC from "../../src/es6/lib/iwc";
 
-import "../../index.js";
+import "../../build/widgets/widget.container.js";
 import { yjsSync } from "../../index.js";
 
 const routes = [
